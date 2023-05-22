2d9851f4354dd6b106560f792d7ae3098dd9f978
rahajengfebri <127112437+rahajengfebri@users.noreply.github.com>
2023-05-22 16:15:05 +0700

    Add files via upload

---
+++ Latihan Modul5/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree pohon = new BinaryTree();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.println("\n Pre Order : ");
+        pohon.preOrder(pohon.root);
+        System.out.println("\n In Order : ");
+        pohon.inOrder(pohon.root);
+        System.out.println("\n Post Order : ");
+        pohon.postOrder(pohon.root);
+
+
+    }
+}
